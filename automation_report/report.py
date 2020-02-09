feccf9f367eb16facff08d326823e2be49fac23d
Bibek Adhikari <bbek_14@yahoo.com>
2020-02-09 21:21:43 +0545

    first commit

---
+++ automation_report/report.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+import string
+import codecs
+testCaseLinks = ""
+testCasecontent = ""
+count = 0
+
+def starttest(case_name):
+    global count, testCaseLinks, testCasecontent
+    count = count + 1
+    testCaseLinks = testCaseLinks + \
+        "<li> <a href='#' class='testname active' id='Test%s'>%s</a> - <span class='PASS casestatus'><i><b>PASS</b> </i></span> </li>" % (
+            count, case_name)
+    testCasecontent = testCasecontent + "<div id='Test%sSteps' class='case'>" % count
+    tableContent = """<table class="table table-hover table-bordered">
+                    <thead>
+                        <th>Status</th>
+                        <th>Message</th>
+                    </thead>
+                    <tbody>"""
+    testCasecontent = testCasecontent + tableContent           
+
+
+def endtest(case_name):
+    global testCasecontent
+    testCasecontent = testCasecontent + "\n </tbody>\n</table>\n</div>"
+
+def success(message):
+    global testCasecontent
+    testCasecontent = testCasecontent + \
+        "<tr class='table-success'><td><i><b>SUCCESS</b></i></td><td> %s </td></tr>\n" % message
+    return testCasecontent
+
+def fail(message):
+    global testCasecontent, testCaseLinks
+    testCaseLinks = testCaseLinks.replace('PASS', 'FAIL')
+    testCasecontent = testCasecontent + \
+        "<tr class='table-danger'><td><i><b>FAIL</b></i></td><td> %s </td></tr>\n" % message
+    return testCasecontent
+
+def info(message):
+    global testCasecontent
+    testCasecontent = testCasecontent + \
+        "<tr class='table-info'><td><i><b>INFO</b></i></td><td> %s </td></tr>\n" % message
+    return testCasecontent
+
+def close():
+    global testCasecontent, testCaseLinks
+    report_file = codecs.open("report.html", 'r', 'utf-8')
+    allcontent = report_file.read()
+    allcontent = allcontent.replace('#testCaseLinks', testCaseLinks)
+    allcontent = allcontent.replace('#testCasecontent', testCasecontent)
+    now = datetime. now()
+    f = open("report" + now.strftime("%m%d%Y%H%M%S") + ".html", "w+")
+    f.write(allcontent)
+    f.close
---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'automation-report',
+  packages = ['automation_report'],
+  version = '0.0.1',
+  license='MIT',
+  description = 'Simple report for your test automation including various cases/steps specifying its valid status.',
+  author = 'Bibek Adhikari',
+  author_email = 'bbekad94@gmail.com',
+  url = 'https://github.com/bibekad123/automation-report',
+  download_url = '',
+  keywords = ['automation report', 'report', 'automation'],  
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+  include_package_data=True,
+)

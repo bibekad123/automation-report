bf58290bbed04fee339849fb0c02c99f22b64ae7
Bibek Adhikari <bbek_14@yahoo.com>
2020-02-10 20:40:52 +0545

    updated filenames

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 setup(
   name = 'automation-report',
   packages = ['automation-report'],
-  version = '0.0.2',
+  version = '0.0.4',
   license='MIT',
   description = 'Simple report for your test automation including various cases/steps specifying its valid status.',
   author = 'Bibek Adhikari',
   author_email = 'bbekad94@gmail.com',
   url = 'https://github.com/bibekad123/automation-report',
-  download_url = 'https://github.com/bibekad123/automation-report/archive/0.0.3.tar.gz',
+  download_url = 'https://github.com/bibekad123/automation-report/archive/0.0.4.tar.gz',
   keywords = ['automation report', 'report', 'automation'],  
   classifiers=[
     'Development Status :: 3 - Alpha',
@@ -16,9 +16,7 @@ setup(
     'Topic :: Software Development',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
   ],
   include_package_data=True,
 )

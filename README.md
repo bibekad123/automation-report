f57010442777dec5957c792cf65ebb87fd3c39c5
Bibek Adhikari <bbek_14@yahoo.com>
2020-03-22 11:52:47 +0545

    made AutomationReport as Class, added total pass, fail counts, modified filename and report title logic, design changes

--- README.md
+++ README.md
@@ -11,26 +11,30 @@ $ pip install automation-report
 ### Code Example
 ```
 # Importing package
-from automation_report import report
+from automation_report.report import AutomationReport
+
+# Create new object for your first test; Report name is passed as a parameter and report is generated with same name
+new_report = AutomationReport("Login Test")
 
 # Use starttest() method to start a new case with its name given as parameter
-report.starttest("CASE 0001: Test the screen")
+new_report.starttest("CASE 0001: Test the screen")
 
 # Populate the various steps status with info(), success(), fail() methods for particular case
-report.info("Page is opened")
-report.success("It works")
-report.fail("Well, it didn't worked.")
+new_report.info("Page is opened")
+new_report.success("Login Successfully")
+new_report.endtest()
 
 # End above started case
-report.endtest()
+new_report.endtest()
 
 # Create yet another case as following
-report.starttest("CASE 0002: Another case")
-report.info("DOnet screen")
-report.success("It works")
-report.endtest()
+new_report.starttest("CASE 0002: Logout")
+new_report.info("User is logged in")
+new_report.success("Logout link is present")
+new_report.fail("User not logged out")
+new_report.endtest()
 # User close method to finally complete whole report generation
-report.close()
+new_report.close()
 ```
 ### Screenshots of report of above code
 ![Automation Report](https://user-images.githubusercontent.com/12621555/74203965-7aabad00-4c99-11ea-9f9f-95323a5c0e63.png)
--- automation_report/report.py
+++ automation_report/report.py
@@ -2,57 +2,74 @@ from datetime import datetime
 import string
 import codecs
 import os
-testCaseLinks = ""
-testCasecontent = ""
-count = 0
 
-def starttest(case_name):
-    global count, testCaseLinks, testCasecontent
-    count = count + 1
-    testCaseLinks = testCaseLinks + \
-        "<li> <a href='#' class='testname active' id='Test%s'>%s</a> - <span class='PASS casestatus'><i><b>PASS</b> </i></span> </li>" % (
-            count, case_name)
-    testCasecontent = testCasecontent + "<div id='Test%sSteps' class='case'>" % count
-    tableContent = """<table class="table table-hover table-bordered">
-                    <thead>
-                        <th>Status</th>
-                        <th>Message</th>
-                    </thead>
-                    <tbody>"""
-    testCasecontent = testCasecontent + tableContent           
+class AutomationReport:
+    testCaseLinks = ""
+    testCasecontent = ""
+    now = datetime.now()
+    report_name = "report" + now.strftime("%m%d%Y%H%M%S")
+    report_file = None
+    case_count = 0
+    total_pass = 0
+    total_fail = 0
+    fail_case = False
 
+    def __init__(self, report_name):
+        self.report_name = report_name
+        self.report_file = open((self.report_name + ".html"), "w+")
 
-def endtest():
-    global testCasecontent
-    testCasecontent = testCasecontent + "\n </tbody>\n</table>\n</div>"
+    def starttest(self, case_name):
+        self.fail_case = 0
+        self.case_count = self.case_count + 1
+        active_class = ""
+        if self.case_count == 1:
+            active_class = 'active' 
+        self.testCaseLinks = self.testCaseLinks + \
+            "<li> <a href='#' class='testname %s' id='Test%s'>%s</a> - <span class='PASS casestatus status-%s'><i><b>PASS</b> </i></span></li>" % (
+                active_class, self.case_count, case_name, self.case_count)
+        self.testCasecontent = self.testCasecontent + \
+            "<div id='Test%sSteps' class='case'>" % self.case_count
+        tableContent = """<table class="table table-hover table-bordered">
+                        <thead>
+                            <th>Status</th>
+                            <th>Message</th>
+                        </thead>
+                        <tbody>"""
+        self.testCasecontent = self.testCasecontent + tableContent
 
-def success(message):
-    global testCasecontent
-    testCasecontent = testCasecontent + \
-        "<tr class='table-success'><td><i><b>SUCCESS</b></i></td><td> %s </td></tr>\n" % message
-    return testCasecontent
+    def endtest(self):
+        self.testCasecontent = self.testCasecontent + "\n </tbody>\n</table>\n</div>"
+        if self.fail_case == True:
+            self.total_fail = self.total_fail + 1
 
-def fail(message):
-    global testCasecontent, testCaseLinks
-    testCaseLinks = testCaseLinks.replace('PASS', 'FAIL')
-    testCasecontent = testCasecontent + \
-        "<tr class='table-danger'><td><i><b>FAIL</b></i></td><td> %s </td></tr>\n" % message
-    return testCasecontent
+    def success(self, message):
+        self.testCasecontent = self.testCasecontent + \
+            "<tr class='table-success'><td><i><b>SUCCESS</b></i></td><td> %s </td></tr>\n" % message
 
-def info(message):
-    global testCasecontent
-    testCasecontent = testCasecontent + \
-        "<tr class='table-info'><td><i><b>INFO</b></i></td><td> %s </td></tr>\n" % message
-    return testCasecontent
+    def fail(self, message):
+        self.fail_case = True
+        self.testCaseLinks = self.testCaseLinks.replace(\
+            "'PASS casestatus status-"+ str(self.case_count) + "'><i><b>PASS",\
+            "'FAIL casestatus status-"+ str(self.case_count) + "'><i><b>FAIL")
+        self.testCasecontent = self.testCasecontent + \
+            "<tr class='table-danger'><td><i><b>FAIL</b></i></td><td> %s </td></tr>\n" % message
 
-def close():
-    global testCasecontent, testCaseLinks
-    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    report_file = codecs.open(os.path.join(__location__, 'report.html'), 'r', 'utf-8')
-    allcontent = report_file.read()
-    allcontent = allcontent.replace('#testCaseLinks', testCaseLinks)
-    allcontent = allcontent.replace('#testCasecontent', testCasecontent)
-    now = datetime. now()
-    f = open("report" + now.strftime("%m%d%Y%H%M%S") + ".html", "w+")
-    f.write(allcontent)
-    f.close
+    def info(self, message):
+        self.testCasecontent = self.testCasecontent + \
+            "<tr class='table-info'><td><i><b>INFO</b></i></td><td> %s </td></tr>\n" % message
+
+    def close(self):
+        __location__ = os.path.realpath(os.path.join(
+            os.getcwd(), os.path.dirname(__file__)))
+        source_report_file = codecs.open(os.path.join(
+            __location__, 'report.html'), 'r', 'utf-8')
+        allcontent = source_report_file.read()
+        allcontent = allcontent.replace('%testCaseLinks%', self.testCaseLinks)
+        allcontent = allcontent.replace('%testCasecontent%', self.testCasecontent)
+        allcontent = allcontent.replace('%headTitle%', self.report_name + " | Automation Report")
+        allcontent = allcontent.replace('%totalCases%', str(self.case_count))
+        allcontent = allcontent.replace('%passedCases%', str(self.case_count - self.total_fail))
+        allcontent = allcontent.replace('%failedCases%', str(self.total_fail))
+        self.report_file.write(allcontent)
+        self.report_file.close()
+
